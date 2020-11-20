eaa277b019174d3da9a2f85e75e3c966b7f2f43c
Mike Miller <emkey1@oneopa.com>
2020-11-20 14:22:29 -0500

    Lots of update

--- login.c
+++ login.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 /* 
- *  Simple login wrapper to force ish to be the user that logs in
+ *  Simple login wrapper to force ish to prompt for username
  */
 
 int main( void ) {
 
-     	char *argv[3] = {"-f ish", NULL};
+   char *argv[2] = {"", NULL}; /* You could try specifying a login here but it won't work */
 
-	int pid = fork();
+   int pid = fork();
 
-	if ( pid == 0 ) {
-		execvp( "/bin/login.real", argv );
-	}
+   if ( pid == 0 ) {
+      printf("Welcome to the AOK iSH root filesystem\n\n");
+      printf("Default login is [36m 'ish'[0m\n\n");
+      execvp( "/bin/login.real", argv );
+   }
 
-	/* Put the parent to sleep for 2 seconds--let the child finished executing */
-	wait( 2 );
+   /* Put the parent to sleep for 2 seconds--let the child finished executing */
+   wait( 2 );
 
-	printf( "Finished executing the parent process\n"
-	        " - the child won't get here--you will only see this once\n" );
-
-	return 0;
+   return 0;
 
 }
 
