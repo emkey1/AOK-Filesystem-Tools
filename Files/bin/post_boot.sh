e4f27209d5455e8906984aea147ba1f495b2e285
jaclu <jacob.lundqvist@gmail.com>
2022-05-24 01:04:05 +0200

    major rewrite

--- Files/bin/post_boot.sh
+++ Files/bin/post_boot.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-#  Version: 1.1.2  2022-05-20
+#  Version: 1.2.0  2022-05-23
 #
 #  Intended usage is for small systems where a cron might not be running and or
 #  needing to do some sanity checks after booting.
@@ -15,45 +15,62 @@
 #    * there are some first-run tasks that need to be done
 #    * services sometimes fail to start by init, restarting them tends to help
 
+respawn_log=/tmp//post_boot.log
+
+
+
+respawn_it() {
+    $0 will_run > $respawn_log 2>&1
+    exit 0
+}
+
+
 
 #
-#  If run with no params, spawn another instance in the background and exit
-#  in order to be inittab friendly
+#  If run with no params, respawn with output going to $respawn_log
+#  all to be inittab friendly
 #
 if [ "$1" = "" ]; then
-    $0 will_run >> /tmp/post_boot.log 2>> /tmp/post_boot.err &
-    exit 0
+    echo "with no param this is respawned, logging to: $respawn_log:"
+    respawn_it
 fi
 
-#
-# Give the system time to complete it's startup
-#
-sleep 10
-
 
 #
-#  Do all sanity checks needed...
+#  /dev/null gets screwed up at times.
+#  Recreate if it needs fixing.
 #
+if [ -z "$(ls -l /dev/null | grep 'root root 1, 3')" ]; then
+    rm /dev/null > /tmp/profile.debug 2>&1
+    mknod /dev/null c 1 3 >> /tmp/profile.debug 2>&1
+    chmod 666 /dev/null >> /tmp/profile.debug 2>&1
+
+    #
+    #  Since /dev/null was recreated respawn this (again),
+    #  in order for the redirects used on this script to work
+    #
+    respawn_it
+fi
+
 
 # The following is needed for upstream PR #1716
 if [ ! -e /dev/fd ]; then
-   ln -s /proc/self/fd /dev/fd
+    ln -s /proc/self/fd /dev/fd
 fi
 
 
 if [[ -e /etc/FIRSTBOOT ]]; then
-   # /dev/null gets screwed up at times.  Recreate just in case
-   # the first time we boot
-   rm /dev/null > /tmp/profile.debug 2>&1
-   mknod /dev/null c 1 3 >> /tmp/profile.debug 2>&1
-   chmod 666 /dev/null >> /tmp/profile.debug 2>&1
 
-   # Start a couple of services
-   rc-update add dcron
-   rc-update add runbg
+    # Start a couple of services
+    rc-update add dcron
+    rc-service dcron restart
+    rc-update add runbg
+    #rc-service runbg restart
 
-   rm /etc/FIRSTBOOT # Only do this stuff once, so remove the file now
+    echo "FIRSTBOOT tasks done"
+    rm /etc/FIRSTBOOT # Only do this stuff once, so remove the file now
 fi
+
 
 # Hack for Alpine 3.14.0
 OS=`/bin/cat /etc/alpine-release`
@@ -64,16 +81,15 @@ if [ $OS = '3.14.0' ]; then
     /usr/local/bin/fix_services
 fi
 
+
 # /etc/init.d/networking keeps getting an extra } written at the end
 # For some unknown reason.  Overwrite it when we login to hopefully
 # mitigate that
 #cp /root/init.d/networking /etc/init.d
 
 
-
-
 #
 #  Restart all services not in started state, should not be needed normally
 #  but here we are, and if they are already running, nothing will happen.
 #
-rc-status |grep -v -e started -e Runlevel:  | awk '{ print $1 }' | xargs -I % /etc/init.d/% restart
+rc-status  |grep -v -e started -e Runlevel:  | awk '{cmd="rc-service " $1 " restart" ; system(cmd)}'
