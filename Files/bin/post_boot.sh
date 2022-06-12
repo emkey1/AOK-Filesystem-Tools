fe0b7af71e905be19b61107cc3aac48481b5cfb3
jaclu <jacob.lundqvist@gmail.com>
2022-06-12 02:31:35 +0200

    Rewrite of log-file handling removed redundant 3.14 check

--- Files/bin/post_boot.sh
+++ Files/bin/post_boot.sh
@@ -1,38 +1,46 @@
 #!/bin/sh
 #
-#  Version: 1.2.1  2022-05-25
+#  Version: 1.3.0  2022-06-11
 #
 #  Intended usage is for small systems where a cron might not be running and or
 #  needing to do some sanity checks after booting.
 #
 #  Trigger this in /etc/inittab by adding a line:
 #
-#  ::wait:/usr/local/bin/post_boot.sh
-#
-#  Before starting /sbin/openrc or similar
+#  ::once:/usr/local/bin/post_boot.sh
 #
 #  In the case of AOK
 #    * there are some first-run tasks that need to be done
-#    * services sometimes fail to start by init, restarting them tends to help
+#    * services sometimes fail to start by init, restarting them here
+#      tends to help
 
-respawn_log=/tmp//post_boot.log
-
-
+post_boot_log=/tmp/post_boot.log
 
 respawn_it() {
-    $0 will_run > $respawn_log 2>&1
+    tmp_log_file="/tmp/post_boot-$$"
+
+    $0 will_run > "$tmp_log_file" 2>&1
+
+    # only keep tmp log if not empty
+    log_size="$(/bin/ls -s "$tmp_log_file" | awk '{ print $1 }')"
+    if [ "$log_size" -ne 0 ]; then
+        echo "---  $(date) ($$)  ---" >> "$post_boot_log"
+        cat "$tmp_log_file" >> "$post_boot_log"
+    fi
+    rm "$tmp_log_file"
+
     exit 0
 }
 
 
-
 #
-#  If run with no params, respawn with output going to $respawn_log
+#  If run with no parameters, respawn with output going to $post_boot_log,
 #  all to be inittab friendly
 #
 if [ "$1" = "" ]; then
-    echo "with no param this is respawned, logging to: $respawn_log:"
+    echo "with no param this is respawned, logging to: $post_boot_log:"
     respawn_it
+    exit 0
 fi
 
 
@@ -42,23 +50,34 @@ fi
 #
 # shellcheck disable=SC2010
 if ! ls -l /dev/null | grep -q "root root 1, 3"; then
+    dev_null_fix_log="/tmp/dev_null_fix.log"
+    echo "Bad /dev/null"
+    ls -l /dev/null
     #
     #  Depending on the nature of the /deb/null issue
     #  and since this scripts output is captured in a log file
     #  The /dev/null issue might cause the logging of the fix
     #  not to happen... Weird but I havent found away arround this
     #
-    rm /dev/null > /tmp/profile.debug 2>&1
-    mknod /dev/null c 1 3 >> /tmp/profile.debug 2>&1
-    chmod 666 /dev/null >> /tmp/profile.debug 2>&1
+    rm /dev/null > "$dev_null_fix_log" 2>&1
+    mknod /dev/null c 1 3 >> "$dev_null_fix_log" 2>> "$dev_null_fix_log"
+    chmod 666 /dev/null >> "$dev_null_fix_log" 2>> "$dev_null_fix_log"
+
+    # only keep fix log if not empty
+    log_size="$(/bin/ls -s "$dev_null_fix_log" | awk '{ print $1 }')"
+    if [ "$log_size" -eq 0 ]; then
+        rm "$dev_null_fix_log"
+    fi
 
     echo "Fixed /dev/null"
+
     #
     #  Since /dev/null was recreated respawn this (again),
     #  in order for the redirects used on this script to work
     #  all logging up this point likely didn't happen.
     #
     respawn_it
+    exit 0
 fi
 
 
@@ -78,16 +97,6 @@ if [ -e /etc/FIRSTBOOT ]; then
 
     echo "FIRSTBOOT tasks done"
     rm /etc/FIRSTBOOT # Only do this stuff once, so remove the file now
-fi
-
-
-# Hack for Alpine 3.14.0
-OS="$(/bin/cat /etc/alpine-release)"
-
-if [ "$OS" = '3.14.0' ]; then
-    # In Alpine 3.14 services do not start up correctly.  Run script to fix 
-    # that if needed
-    /usr/local/bin/fix_services
 fi
 
 
