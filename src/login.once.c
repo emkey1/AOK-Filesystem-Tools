9db02e39d04e64970adb607300e49d85cced112b
Mike Miller <emkey1@gmail.com>
2023-02-23 05:57:06 -0800

    Update login.once.c
    Use new "functions.h"

--- src/login.once.c
+++ src/login.once.c
@@ -1,44 +1,19 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include "functions.h"
 
-void show_issue(void)
-{
-   FILE *ptr;
-   char ch;
-
-   if (access("/etc/issue", F_OK) == 0)
-   {
-      // Opening file in reading mode
-      ptr = fopen("/etc/issue", "r");
-
-      // Printing what is written in file
-      // character by character using loop.
-      do
-      {
-         ch = fgetc(ptr);
-         printf("%c", ch);
-
-         // Checking if character is not EOF.
-         // If it is EOF stop reading.
-      } while (ch != EOF);
-
-      // Closing the file
-      fclose(ptr);
-   }
-}
+extern void show_issue(void);
 
 /*
  *  Simple login wrapper to force ish to prompt for username
  */
 
-int main(void)
-{
+int main(void) {
    char *argv[2] = {"login", NULL}; // You could try specifying a login here but it won't work
    int pid = fork();
 
-   if (pid == 0)
-   {
+   if (pid == 0) {
       show_issue();
       execvp("/bin/busybox", argv);
    }
