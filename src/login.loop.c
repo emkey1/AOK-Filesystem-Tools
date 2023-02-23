6244a6911945997721ca88cadd83a28c76768ff6
Mike Miller <emkey1@gmail.com>
2023-02-23 05:55:47 -0800

    Update login.loop.c

--- src/login.loop.c
+++ src/login.loop.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include "functions.h"
 
 
 /*
@@ -10,40 +11,14 @@
  *  system would do
  */
 
-void show_issue(void)
-{
-    FILE *ptr;
-    char ch;
+extern void show_issue(void);
 
-    if (access("/etc/issue", F_OK) == 0)
-    {
-        // Opening file in reading mode
-        ptr = fopen("/etc/issue", "r");
-
-        // Printing what is written in file
-        // character by character using loop.
-        do
-        {
-            ch = fgetc(ptr);
-            printf("%c", ch);
-
-            // Checking if character is not EOF.
-            // If it is EOF stop reading.
-        } while (ch != EOF);
-
-        // Closing the file
-        fclose(ptr);
-    }
-}
-
-int mylogin(void)
-{
+int mylogin(void) {
 
     char *argv[2] = {"login", NULL}; // You could try specifying a login here but it won't work
     int pid = fork();
 
-    if (pid == 0)
-    {
+    if (pid == 0) {
         show_issue();
         execvp("/bin/busybox", argv);
     }
@@ -52,10 +27,8 @@ int mylogin(void)
     wait(NULL);
 }
 
-int main(void)
-{
-    while (1)
-    {
+int main(void) {
+    while (1) {
         mylogin();
     }
 
