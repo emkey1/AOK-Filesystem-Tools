41bb8b6065e98715cc57e7689622d8059af564d5
jaclu <jacob.lundqvist@gmail.com>
2023-03-26 01:56:47 +0100

    obsolete foregotten files

--- Debian/bin/login.loop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-while true; do
-    [ -f /etc/issue ] && cat /etc/issue
-    /bin/login.original
-done
--- Debian/bin/login.once.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-[ -f /etc/issue ] && cat /etc/issue
-/bin/login.original
