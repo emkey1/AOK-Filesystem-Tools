3eddcc3483fad22cbdc3f4e6f919e5f0d372816e
jaclu <jacob.lundqvist@gmail.com>
2023-06-05 00:50:06 +0200

    fixed handling of custom files

--- custom/custom_files.sh
+++ custom/custom_files.sh
@@ -14,24 +14,30 @@ process_custom_file_list() {
 
     msg_2 "process_custom_file_list($files_template)"
     source "$files_template"
-    # Iterate over the array
-    for row in "${file_list[@]}"; do
-        if [ "${#row[@]}" -ne 3 ]; then
-            error_msg "$files_template - \
-                line should contain 3 items, found: $row"
-        fi
 
-        src_file="${row[0]}"
-        dst_file="${row[1]}"
-        owner="${row[2]}"
+    # Iterate over the array
+    while [ ${#file_list[@]} -gt 0 ]; do
+        src_file="${file_list[0]}"
+        dst_file="${file_list[1]}"
+        owner="${file_list[2]}"
+
+        #  param checks
+        [ -z "$src_file" ] && error_msg "process_custom_file_list() src_file empty"
+        [ -z "$dst_file" ] && error_msg "process_custom_file_list() dst_file empty"
 
         if [ ! -f "$src_file" ]; then
             error_msg "$files_template, src_file not found: $src_file"
         fi
         cp -av "$src_file" "$dst_file"
-        chown "$owner" "$dst_file"
+        if [ -n "$owner" ]; then
+            msg_3 "Changing ownership for $dst_file to: $owner"
+            chown "$owner" "$dst_file"
+        fi
+
+        #  Remove the first item from the array
+        file_list=("${file_list[@]:3}")
     done
-    msg_3 "process_custom_file_list($files_template) - done"
+    # msg_3 "process_custom_file_list($files_template) - done"
 }
 
 if is_alpine && [[ -n "$ALPINE_CUSTOM_FILES_TEMPLATE" ]]; then
--- custom/custom_files_template.sh
+++ custom/custom_files_template.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 # dummy shebang, helping linters, this file is sourced
 
-
 #
 #  This is a template, If you copy this to /iCloud or other location
-#  available for the destination device, you dont have to mess with the
+#  available on the destination device, you dont have to mess with the
 #  git repo
 #
-
+# Each item is: src_file dest_file owner
+# if owner is "" then owner is not set
+#
+#  shellcheck disable=SC2034
 file_list=(
-    # Each item is (src_file, dest_file, owner)
-    # if owner is "" then root:root is used
-    ("/iCloud/ish_config/files/etc_hosts" "/etc/hosts" "")
+    /iCloud/deploy/files/etc_hosts /etc/hosts root:
 )
 
 #
@@ -19,13 +19,6 @@ file_list=(
 #
 if [[ "$(hostname)" = "JacPad" ]]; then
     file_list+=(
-        ("/iCloud/ish_config/files/jacpad/ssh_host_dsa_key"
-         "/etc/ssh/ssh_host_dsa_key" "")
-        ("/iCloud/ish_config/files/jacpad/ssh_host_dsa_key.pub"
-         "/etc/ssh/ssh_host_dsa_key.pub" "")
-        ("/iCloud/ish_config/files/jacpad/ssh_host_ecdsa_key"
-         "/etc/ssh/ssh_host_dsa_key" "")
-        ("/iCloud/ish_config/files/jacpad/ssh_host_ecdsa_key.pub"
-         "/etc/ssh/ssh_host_dsa_key.pub" "")
+        /iCloud/deploy/files/jacpad/ssh_host_dsa_key /etc/ssh/ssh_host_dsa_key root:
     )
 fi
