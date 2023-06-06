e8cf05acb2db35802c94d29f0cbd2668f009b487
jaclu <jacob.lundqvist@gmail.com>
2023-06-06 12:19:19 +0200

    added some comments about logic for tmux conf

--- common_AOK/custom/keyboarding.sh
+++ common_AOK/custom/keyboarding.sh
@@ -16,7 +16,7 @@ define_tmux_esc() {
         echo "ERROR: define_tmux_esc() - no param"
         exit 1
     fi
-    
+
     echo "tmux Using [$esc_sequence]"
 
     $TMUX_BIN set -s user-keys[200] "$esc_sequence" # multiKeyBT
@@ -36,21 +36,39 @@ define_tmux_esc() {
 #
 #===============================================================
 
-tmux_esc_indicator="/etc/opt/tmux_esc_prefix"
+# Set env variable across ssh sessions so that remote tmux sessions
+# honor the same nav key handling
+# Would it need unbind in case of change nav handling?
+# no tmux should be restarted
+#
+# tmux source a conf file dedicated to handle this, sample lines
+#
+# bind -N "S-Up = PageUp"     -n  S-Up     send-keys PageUp
+# bind -N "C-Up = PageUp"     -n  S-Up     send-keys PageUp
+# bind -N "M-Up = PageUp"     -n  S-Up     send-keys PageUp
+#
+# set -s user-keys[200]  "{key_oct}"  # multiKeyNav
+#
+# bind -n User200 switch-client -T multiKeyNav
+# bind -T multiKeyNav  User200  send Escape
+# bind -T multiKeyNav  Down     send PageDown
+# bind -T multiKeyNav  Up       send PageUp
+# bind -T multiKeyNav  Left     send Home
+# bind -T multiKeyNav  Right    send End
 
-ios_keyboard=/etc/opt/tmux_esc_prefix
+tmux_nav_key_handling="/etc/opt/tmux_nav_key_handling"
 
-if [ ! -f "$tmux_esc_indicator" ]; then
+if [ ! -f "$tmux_nav_key_handling" ]; then
     #  No BT keyb defined for Escape handling
     exit 0
 fi
 
-if [ -z "$TMUX_BIN" ]; then
+if [ -z "$TMUX" ]; then
     #  For now this can only be used inside tmux
     exit 0
 fi
 
-tmux_esc_char="$(cat $tmux_esc_indicator)"
+nav_key_handling="$(cat $tmux_nav_key_handling)"
 
 #  Only run if esc is defined
-[ -n "$tmux_esc_char" ] && define_tmux_esc "$tmux_esc_char"
+[ -n "$nav_key_handling" ] && define_tmux_esc "$nav_key_handling"
