4cf2a1441a62f1f5ec36e086973a6e2287ba63e8
jaclu <jacob.lundqvist@gmail.com>
2023-06-17 08:51:57 +0200

    fix esc-char handling for older versions of tmux

--- common_AOK/usr_local_bin/nav_keys.sh
+++ common_AOK/usr_local_bin/nav_keys.sh
@@ -46,7 +46,11 @@ tmux_mod_arrow() {
 }
 
 tmux_esc_prefix() {
-    sequence="$1"
+    #
+    #  At least Esc must be converted from octal to special char
+    #  for older tmux versions. Doesnt hurt on newer
+    #
+    sequence="$(echo $1 | sed 's/\\033/\\e/g')"
     if [ "$sequence" = " " ]; then
         echo "No special tmux Escape handling requested"
         clear_nav_key_usage
