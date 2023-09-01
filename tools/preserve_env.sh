36edd800a87a3368113635cd7a074d2f14c8be86
Jacob Lundqvist <Jacob.Lundqvist@gmail.com>
2023-09-01 02:32:37 +0200

    explained purpose

--- tools/preserve_env.sh
+++ tools/preserve_env.sh
@@ -1,24 +1,36 @@
 #!/bin/sh
-
 #
-#  Internally all the parts of AOK assumes TMPDIR has been set
-#  Since many tools needs to be run as root, they will source
+#  shellcheck disable=SC2154
+#
+#  Part of https://github.com/emkey1/AOK-Filesystem-Tools
+#
+#  License: MIT
+#
+#  Purpose of this is to ensure TMPDIR is assigned.
+#  It defins where most actions on the host platform will be performed.
+#
+#  It will be assigned as follows:
+#
+#  1) If AOK_TMPDIR is defined in the env,it will be used
+#  2) If TMPDIR is defined in the env, it will be used as is
+#  3) It will be set to /tmp
+#
+#  Internally all the parts of AOK assumes TMPDIR has been set.
+#  Since most tools needs to be run as root, they will source
 #  tools/run_as_root.sh thereby restarting as root if not so already.
+#  That one sources this, so the only times this would need to
+#  be directly sourced, is tools not needing to be run as root.
 #
-#  In case AOK should use a separate TMPDIR, this can be set in AOK_TMPDIR
-#  if found TMPDIR will use this.
-#  After this point AOK_TMPDIR as such is no longer needed,
-#  and if something is sudoed by run_as_root.sh AOK_TMPDIR is not preserved
-#
-#  For this to work as expected relevant env settings need to be supplied
-#  when something is re-run using sudo.
+#  tools/run_as_root.sh will filter out AOK_TMPDIR before restarting
+#  items, otherwise it would risk spilling over when chrooting,
+#  for example.
 #
 
 # echo ">>> preserve_env.sh starting"
-
 [ -z "$AOK_DIR" ] && {
     echo "ERROR: tools/preserve_env.sh needs AOK_DIR set to base dir of repo"
     exit 1
 }
 
 TMPDIR="${AOK_TMPDIR:-${TMPDIR:-/tmp}}"
+unset AOK_TMPDIR
