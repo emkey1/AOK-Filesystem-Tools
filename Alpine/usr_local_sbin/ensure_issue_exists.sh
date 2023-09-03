15ddb91f5abf9e323fba6ac4a4fc669967ce03ed
Jacob Lundqvist <Jacob.Lundqvist@gmail.com>
2023-09-03 14:04:09 +0200

    added header

--- Alpine/usr_local_sbin/ensure_issue_exists.sh
+++ Alpine/usr_local_sbin/ensure_issue_exists.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-
+#
+#  shellcheck disable=SC2154
+#
+#  Part of https://github.com/jaclu/AOK-Filesystem-Tools
+#
+#  License: MIT
 #
 #  Login crashes if /etc/issue is not pressent.
 #  This ensures at least an empty file is there
