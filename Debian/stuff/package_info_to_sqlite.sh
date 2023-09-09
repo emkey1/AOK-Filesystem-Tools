3e97fca16e68d32c17b16534217d7769c9c8bf86
Jacob Lundqvist <Jacob.Lundqvist@gmail.com>
2023-09-10 00:16:58 +0200

    Added more info on img generation

---
+++ Debian/stuff/package_info_to_sqlite.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+# sqlite3 /tmp/package_info.db 'select priority,section,name,is_leaf FROM packages ORDER By priority,section,is_leaf,name'
+
+
+#
+#  Sample usages
+#
+
+declare -A packages_by_priority_section
+declare -A priority_order
+
+# Define priority severity order
+priority_order=(
+    [required]=1
+    [important]=2
+    [standard]=3
+    [optional]=4
+    [extra]=5
+)
+
+# Create an SQLite database or connect to an existing one
+
+database="/tmp/package_info.db"
+
+rm -f "$database"
+
+sqlite3 "$database" <<EOF
+CREATE TABLE IF NOT EXISTS packages (
+    priority TEXT,
+    section TEXT,
+    name TEXT,
+    is_leaf TEXT
+);
+EOF
+
+echo ">>> db created"
+
+# Function to print a progress dot
+print_progress_dot() {
+    echo -n "."
+}
+
+#
+#  Checking dependencies inside iSH would take longer than forever,
+#  so on iSH skip this check
+#
+if [ ! -d /proc/ish ]; then
+    check_dependencies=1
+fi
+
+# Iterate over all installed packages
+for package in $(dpkg-query -f '${Package}\n' -W); do
+    #
+    #  Check if the package has dependencies
+
+    is_leaf=""  #  Defaults to not be a leaf
+    if [ "$check_dependencies" = 1 ]; then
+	# If the package has no dependencies, it's a leaf package
+	if [ -z "$(apt-cache depends "$package" 2>/dev/null | grep "Depends:")" ]; then
+	    is_leaf="*"
+	fi
+    fi
+
+    # Get package section and priority
+    section=$(dpkg-query -f '${Section}\n' -W "$package")
+    priority=$(dpkg-query -f '${Priority}\n' -W "$package")
+
+    # Store the package in the appropriate array
+    sqlite3 "$database" "INSERT INTO packages (priority, section, name, is_leaf) \
+	VALUES ('$priority', '$section', '$package', '$is_leaf');"
+    
+    print_progress_dot
+done
+
+echo "Package information has been stored in '$database'."
